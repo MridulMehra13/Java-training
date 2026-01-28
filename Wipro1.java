f4d823ec60b213d540f6ffd2f9fa91daaa3519a4
MRIDUL <mridulmehra3@gmail.com>
2026-01-28 15:18:34 +0530

    commit msg

---
+++ Wipro1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Wipro1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        findDuplicateElements(arr);
+
+        int number = 3;
+        System.out.println("Square of " + number + " is: " + square(number));
+        System.out.println("Factorial of " + number + " is: " + factorial(number));
+    }
+
+    public static void findDuplicateElements(int arr[]) {
+        System.out.println("The duplicate elements in the array are:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println(arr[i]);
+                    break;
+                }
+            }
+        }
+        
+    }
+
+    public static int square(int n) {
+        return n * n;
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+}
